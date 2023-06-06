77828dc7750238a5515dc6b0f5b34912d31c28c3
Spencer A Warren <spencer.warren.19@cnu.edu>
2023-06-06 14:08:36 -0400

    Create Springboot Project

---
+++ BackEnd/src/main/java/com/Spencer/MealPlanning/MealPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.Spencer.MealPlanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealPlanningApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/Spencer/MealPlanning/MealPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Spencer.MealPlanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
